2559b0d460bf13a84ac88db8346dc85d1c2b98b2
Saeful Haq Faruqi <139915808+saefulfaruqi@users.noreply.github.com>
2023-10-23 17:41:14 +0000

    want to lear react context

---
+++ src/page/LearnUseContext.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LearnUseContext = () => {
+    return (
+        <div>
+            <h1>Learn useContext Hook</h1>
+            <p>Some content here...</p>
+        </div>
+    );
+}
+
+export default LearnUseContext;
